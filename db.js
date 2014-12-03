0e3e7c037c7176b1392caddee4c4ef30f46f1002
Raghav Munjal <munjal.raghav@gmail.com>
2014-12-02 23:06:13 -0800

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var mysql =	require('mysql');
-var db=require('/srv/www/cmpe281/shared/config/opsworks');
+var db=require('/srv/www/cmpe282/shared/config/opsworks');
 db.db.user="root";
 db.db.multipleStatements=true;
 var connectionpool = mysql.createPool(db.db);
